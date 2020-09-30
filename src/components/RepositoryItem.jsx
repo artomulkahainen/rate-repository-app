19272f5631e8a68e9844b8bf28257c78eddedd9c
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2020-09-30 09:07:35 +0300

    osa10

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RepositoryItem = ({
+  id,
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  ratingAverage,
+  reviewCount,
+  ownerAvatarUrl,
+}) => {
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
